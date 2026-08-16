d11b6016681eefb605600504015eaa2627e3fd05
Lecoo <wladpiq451@gmail.com>
2026-08-16 18:20:20 +0400

    Добавил MAUI

---
+++ Go.Mobile/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Go.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "Go.Mobile" };
+        }
+    }
+}
---
+++ Go.Mobile/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Go.Mobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
