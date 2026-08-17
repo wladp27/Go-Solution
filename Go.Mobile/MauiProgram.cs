189264ec5837aca5ad468e1e37b287beeea5f423
Lecoo <wladpiq451@gmail.com>
2026-08-17 13:17:46 +0400

    исправил предупреждения

--- Go.Mobile/MauiProgram.cs
+++ Go.Mobile/MauiProgram.cs
@@ -27,8 +27,8 @@ namespace Go.Mobile
             builder.Services.AddScoped<AuthenticationStateProvider>(provider =>
                 provider.GetRequiredService<AuthenticationStateProviderCustom>());
             builder.Services.AddScoped<AuthorizationMessageHandler>();
-            builder.Services.AddHttpClient("TokenAPIClient", client => client.BaseAddress = new Uri("https://localhost:7065")).AddHttpMessageHandler<AuthorizationMessageHandler>();
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7065") });
+            builder.Services.AddHttpClient("TokenAPIClient", client => client.BaseAddress = new Uri("http://201.34.131.23:8080")).AddHttpMessageHandler<AuthorizationMessageHandler>();
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://201.34.131.23:8080") });
             builder.Services.AddMediatR(cfg =>
             {
                 MediatRServiceConfiguration mediatRServiceConfiguration = cfg.RegisterServicesFromAssembly(typeof(GoWeb.Shared.Service.CityService).Assembly);
