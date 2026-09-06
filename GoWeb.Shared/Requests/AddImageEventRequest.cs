da7d87aac045693510ffdba2772b918fa66fd9ed
Владислав <wladpiq451@gmail.com>
2026-09-06 21:01:58 +0400

    запрос на загрузку изображения

---
+++ GoWeb.Shared/Requests/AddImageEventRequest.cs
@@ -0,0 +1,27 @@
+﻿using GoWeb.Shared.Model;
+using MediatR;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace GoWeb.Shared.Requests
+{
+    public record AddImageEventRequest(IBrowserFile EventImage) : IRequest<AddImageEventRequest.Response>
+    {
+        public const string RouteTemplate = "/api/images/add";
+        public class Response : OperationResult<string>
+        {
+            [JsonConstructor]
+            public Response(bool isSuccess, string data, string errorMessage)
+                : base(isSuccess, data, errorMessage) { }
+
+            public new static Response Success(string value) =>
+                new(true, value, string.Empty);
+
+            public new static Response Failure(string errorMessage) =>
+                new(false, default!, errorMessage);
+        }
+    }
+}
