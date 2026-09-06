1739e33f63bc01c595e21fc83e914be8241ba865
Владислав <wladpiq451@gmail.com>
2026-09-06 21:02:15 +0400

    лишняя строка

--- GoWeb.Shared/Features/ManageEvent/Handlers/CreateLocationHandler.cs
+++ GoWeb.Shared/Features/ManageEvent/Handlers/CreateLocationHandler.cs
@@ -25,7 +25,6 @@ namespace GoWeb.Shared.Features.ManageEvent.Handlers
                     var errorContent = await httpResponse.Content.ReadFromJsonAsync<CreateLocationRequest.Response>(cancellationToken);
                     return errorContent ??  CreateLocationRequest.Response.Failure("Произошла непредвиденная ошибка");
                 }
-                string jsonString = await httpResponse.Content.ReadAsStringAsync(cancellationToken);
                 var content = await httpResponse.Content.ReadFromJsonAsync<CreateLocationRequest.Response>(cancellationToken);
                 return content ?? CreateLocationRequest.Response.Failure("Получен пустой ответ от сервера");
             }
