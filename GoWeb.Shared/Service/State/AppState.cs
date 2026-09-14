500f7209b576c4a948fc2e4bab09db51c31e7c29
Владислав <wladpiq451@gmail.com>
2026-09-14 19:01:28 +0400

    Создал сервис состояний во время сессии

---
+++ GoWeb.Shared/Service/State/AppState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoWeb.Shared.Service.State
+{
+    public class AppState
+    {
+        public NewEventState NewEventState { get; } 
+
+        public AppState()
+        {
+            NewEventState = new NewEventState();
+        }
+
+    }
+}
---
+++ GoWeb.Shared/Service/State/NewEventState.cs
@@ -0,0 +1,16 @@
+﻿using GoWeb.Shared.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoWeb.Shared.Service.State
+{
+    public class NewEventState
+    {
+        private EventDTO _unsavedNewEvent = new();
+        public void SaveEvent(EventDTO newEvent)=> _unsavedNewEvent = newEvent;
+        public EventDTO GetEvent() => _unsavedNewEvent;
+        public void ClearEvent() => _unsavedNewEvent = new();
+
+    }
+}
