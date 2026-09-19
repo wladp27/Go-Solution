44e0e8733dda36312adeaa36018316d07d25a64f
Владислав <wladpiq451@gmail.com>
2026-09-19 17:20:58 +0400

    Исправлен баг приводящий к исключение при работе с компонентом слушающий EditContext

--- GoWeb.Shared/Сonstants/ImagePath.cs
+++ GoWeb.Shared/Сonstants/ImagePath.cs
@@ -7,7 +7,7 @@ namespace GoWeb.Shared.Сonstants
     public static class ImagePath
     {
         public const string HttpBase = "http://201.34.131.23:8080";
-        public static readonly string Image = $"{HttpBase}/images/events/";
+        public static readonly string Event = $"{HttpBase}/images/events/";
         public static readonly string EventTypes = $"{HttpBase}/images/eventsTypes/";
     }
 }
